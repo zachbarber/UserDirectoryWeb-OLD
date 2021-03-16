ee261b57a54dc31bee5e86df5afa8cdf38ed26e9
Zach Barber <zachbarber321@gmail.com>
2021-03-15 21:38:51 -0400

    add sidebar styling

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,24 +10,35 @@ class Sidebar extends React.Component {
         this.state = {
             isExpanded: true,
         }
+
+        this.sidebarButtonOnClick = this.sidebarButtonOnClick.bind(this);
+    }
+
+    sidebarButtonOnClick() {
+
+        this.setState({
+            isExpanded: !this.state.isExpanded
+        })
     }
 
     render() {
 
+        const { isExpanded } = this.state;
+
         return (
             <>
-                <div className='sidebarContainer'>
-                    {
-                        this.state.isExpanded ?
-                            <div className='listContainer'>
-                            <ul className='sidebarList'>
-                                <li className='sidebarListItem'>Link 1</li>
-                                <li className='sidebarListItem'>Link 2</li>
-                                <li className='sidebarListItem'>Link 3</li>
-                            </ul>
-                            </div>
-                            : 
-                    }
+                <div className={isExpanded ? 'sidebarContainerExpanded' : 'sidebarContainerCollapsed'} >
+                    <div className={isExpanded ? 'sidebarListContainerExpanded' : 'sidebarListContainerCollapsed'} >
+                        <ul className={isExpanded ? 'sidebarListExpanded' : 'sidebarListCollapsed'} >
+                            <li className={isExpanded ? 'sidebarListItemExpanded' : 'sidebarListItemCollapsed'}>Link 1</li>
+                            <li className={isExpanded ? 'sidebarListItemExpanded' : 'sidebarListItemCollapsed'}>Link 2</li>
+                            <li className={isExpanded ? 'sidebarListItemExpanded' : 'sidebarListItemCollapsed'}>Link 3</li>
+                        </ul>
+                    </div>
+
+                    <div className='sidebarButtonContainer'>
+                        <img className='sidebarButton' onClick={this.sidebarButtonOnClick} src={isExpanded ? `${process.env.PUBLIC_URL}/leftArrow.png` : `${process.env.PUBLIC_URL}/rightArrow.png`} alt='sidebarArrow'/>
+                    </div>
                 </div>
             </>
         )
